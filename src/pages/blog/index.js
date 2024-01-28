ba85b99196c7d87a92067dd22f4a8c2db19876c8
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2024-01-28 00:27:31 -0500

    Adding some styling. Not much done yet, but it's looking good so far 👍🏾

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,10 +1,34 @@
 import * as React from "react";
+import { graphql, Link } from "gatsby";
 import Layout from "../../components/layout.js";
 
-export default function LWMIndex() {
+export default function BlogIndex({ data }) {
+  const nodes = data.allMdx.nodes;
   return (
     <Layout>
-      <h1>Blog</h1>
+      {nodes.map((node) => {
+        return (
+          <div key={node.id}>
+            <h2 className="content-link"><Link to={node.frontmatter.slug}>{node.frontmatter.title}</Link></h2>
+              {node.frontmatter.date}
+          </div>
+        );
+      })}
     </Layout>
   );
 }
+
+export const data = graphql`
+  query {
+    allMdx {
+      nodes {
+        id
+        frontmatter {
+          title
+          slug
+          date(formatString: "DD MMMM, YYYY")
+        }
+      }
+    }
+  }
+`;
