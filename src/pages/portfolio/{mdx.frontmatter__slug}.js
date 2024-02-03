fb8a34e08a472b718b96b78ddd3c8744049306df
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2024-02-03 02:56:22 -0500

    Fixed graphql query.

--- src/pages/portfolio/{mdx.frontmatter__slug}.js
+++ src/pages/portfolio/{mdx.frontmatter__slug}.js
@@ -1,7 +1,29 @@
+import { graphql } from "gatsby";
 import * as React from "react";
+import Layout from "../../base-components/layout";
 
-export default function PortfolioPost() {
-    return (
-        <h1>Portfolio Post</h1>
-    );
+export default function PortfolioPost({ data }) {
+  let mdxNode = data.mdx;
+
+  return (
+    <Layout>
+      <div>
+        <h1>{mdxNode.frontmatter.title}</h1>
+        <div>{mdxNode.body}</div>
+      </div>
+    </Layout>
+  );
 }
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      body
+      frontmatter {
+        title
+        slug
+      }
+    }
+  }
+`;
