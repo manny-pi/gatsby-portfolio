e819ea867a70bcec6475b51e5783effe1e385c59
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2024-02-03 00:47:56 -0500

    Decomposed layout.

---
+++ src/base-components/header.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { ButtonGroup, Button } from "@material-tailwind/react";
+
+function Header(props) {
+    return (
+      <div id="header">
+        <ButtonGroup variant="outlined">
+          <Button>
+            <Link to="/">Home</Link>
+          </Button>
+          <Button>
+            <Link to="/blog">Blog</Link>
+          </Button>
+          <Button>
+            <Link to="/learn-with-me">Learn With Me</Link>
+          </Button>
+          <Button>
+            <Link to="/portfolio">Portfolio</Link>
+          </Button>
+        </ButtonGroup>
+      </div>
+    );
+  }
+  
--- src/base-components/layout.js
+++ src/base-components/layout.js
@@ -1,27 +1,6 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import { ButtonGroup, Button } from "@material-tailwind/react";
+import Header from "./header.js";
 
-function Header(props) {
-  return (
-    <div id="header">
-      <ButtonGroup variant="outlined">
-        <Button>
-          <Link to="/">Home</Link>
-        </Button>
-        <Button>
-          <Link to="/blog">Blog</Link>
-        </Button>
-        <Button>
-          <Link to="/learn-with-me">Learn With Me</Link>
-        </Button>
-        <Button>
-          <Link to="/portfolio">Portfolio</Link>
-        </Button>
-      </ButtonGroup>
-    </div>
-  );
-}
 
 export default function Layout({ children }) {
   return (
