a43affb53f1b6032a32e8dd6bd75c84a4c930483
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2024-08-27 08:23:26 -0400

    Added a tag selector to the blog page.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,14 +2,49 @@ import { graphql, Link } from "gatsby";
 import * as React from "react";
 import Layout from "../../base-components/layout.js";
 
+function TagSelector({ nodes }) {
+  // Get all the tags for the blog posts
+  let allTags = [];
+  let tags;
+  {
+    nodes.forEach((node) => {
+      // Handle cases where there are no tags on the post yet.
+      try {
+        tags = node.frontmatter.tags;
+        tags.forEach((tag) => {
+          if (!allTags.includes(tag)) {
+            allTags.push(tag);
+          }
+        });
+      } catch (e) {
+        /* Do nothing */
+      }
+    });
+  }
+
+  return (
+    <div>
+      <select name="tags">
+        {allTags.map((tag) => {
+          return (<option value={tag}>{tag}</option>);
+        })}
+      </select>
+    </div>
+  );
+}
+
 export default function BlogIndex({ data }) {
   const nodes = data.allMdx.nodes;
+
   // Sort blog posts by date
   // let entries = Object.entries(nodes).sort((p1, p2) => {(new Date(p1[1].date)) - (new Date(p2[1].date))});
   // let sortedEntries = Object.fromEntries(entries);
 
   return (
     <Layout>
+      <div>
+        <TagSelector nodes={nodes} />
+      </div>
       {nodes.map((node) => {
         let fm = node.frontmatter;
         return (
@@ -39,6 +74,7 @@ export const data = graphql`
           title
           slug
           date
+          tags
         }
       }
     }
