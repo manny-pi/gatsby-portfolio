309ce2f98dcd570f9a67ec670fe7455df1a07c1f
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2024-10-06 09:07:11 -0400

    Updated the learn with me layout

--- src/pages/learn-with-me/index.js
+++ src/pages/learn-with-me/index.js
@@ -1,24 +1,31 @@
+import { graphql, Link } from "gatsby";
 import * as React from "react";
 import Layout from "../../components/layout.js";
-import { graphql } from "gatsby";
-import { Link } from "gatsby";
 
-export default function LWMIndex({ data }) {
+export default function BlogIndex({ data }) {
   const nodes = data.allMdx.nodes;
+
   return (
     <Layout>
-      {nodes.map((node) => {
-        let fm = node.frontmatter;
-        return (
-          <div key={node.id}>
-            <div className="blog-content-card">
-              <h2 className="content-link"><Link to={fm.slug}>{fm.title}</Link></h2>
-              <h3>{fm.date}</h3>
-              <p><em>{node.excerpt}</em></p>
+      {/* <div>
+        <TagSelector nodes={nodes} />
+      </div> */}
+      <div>
+        {nodes.map((node) => {
+          let fm = node.frontmatter;
+          return (
+            <div key={node.id}>
+              <div className="blog-content-card">
+                <h2 className="content-link">
+                  <Link to={fm.slug}>{fm.title}</Link>
+                </h2>
+                <p>{fm.date}</p>
+                <p><em>{fm.description}</em></p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </Layout>
   );
 }
